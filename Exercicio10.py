5ef09fb3a5c7f2657cf2db29dec617acfaccb774
Tiago547-ciber <136295383+Tiago547-ciber@users.noreply.github.com>
2025-06-03 19:20:06 -0300

    Add files via upload
    Exercise number ten in the Video Course serie

---
+++ Exercicio10.py
@@ -0,0 +1,10 @@
+
+#Exercicio 10
+#CONVERSOR DE MOEDAS
+print('Quanto dinheiro você tem na carteira?')
+
+dolar = 5.64
+
+real = input('R$ ')
+convert = float(real) * dolar
+print('Com ' + real + ' você pode comprar US$' + str(convert))
